928f06c67f29997f29c7584429b248a68381203b
Donald <dniskhe@gmail.com>
2024-10-24 16:36:46 +0100

    Code Commit

--- quizProject/settings.py
+++ quizProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-md9)m14&w((!!f(ai2ruok*+bawud0afj)%##q4%^$$-di^ost
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
